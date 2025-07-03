c3f511ba283df949ffc048ab0f6849509899aabb
jyn <github@jyn.dev>
2025-07-02 23:06:15 -0400

    add "computer of the future" taxonomy

--- new_post.sh
+++ new_post.sh
@@ -19,8 +19,11 @@ date = $date
 draft = true
 #description: \"\"
 #taxonomies.tags = []
+#taxonomies.computer-of-the-future = [\"0\"]
 #extra.audience = \"everyone\"
----
+#extra.unlisted = true
+#extra.stub = true
++++
 " > "$dst"
 else
     echo "updating date for existing post $(realpath "$dst")"
